f3a46afca97a9e6cf6498bc7526046a33a8f507b
samirMr <sbensghaier@maelstrom-research.org>
2020-02-12 16:42:42 -0500

    Update Last release Drupal core 7.69 Obiba Mica 36.2

--- bin/start.sh
+++ bin/start.sh
@@ -4,7 +4,7 @@
 if [ -n $MYSQL_HOST ]
 	then
 	# Wait for MySQL to be ready
-	until mysql -h $MYSQL_HOST -u root -p$MYSQL_PASSWORD -e ";" &> /dev/null
+	until mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -e ";" &> /dev/null
 	do
 	  sleep 1
 	done
@@ -24,16 +24,16 @@ cd /var/www/html && \
   chown -R www-data:www-data .
 
 # Set mica_url and agate url
-if [ -n $MICA_PORT ]
+if [ -n $MICA_PORT_8445_TCP_ADDR ]
   	then
   	cd /var/www/html && \
-  	  drush vset -y mica_url https://$MICA_ADDR:$MICA_PORT
+  	  drush vset -y mica_url https://$MICA_PORT_8445_TCP_ADDR:$MICA_PORT
   fi
 
-  if [ -n $AGATE_PORT ]
+  if [ -n $AGATE_PORT_8444_TCP_ADDR ]
   	then
   	cd /var/www/html && \
-  	  drush vset -y agate_url https://$AGATE_ADDR:$AGATE_PORT
+  	  drush vset -y agate_url https://$AGATE_PORT_8444_TCP_ADDR:$AGATE_PORT
   fi
 
 apache2-foreground
